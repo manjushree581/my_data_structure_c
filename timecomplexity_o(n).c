c646dd2920b77d25b9b4cd928c98de062e159778
manjushree581 <manjushreepb14@gmail.com>
2025-11-03 12:46:01 +0530

    Create timecomplexity_o(n).c

---
+++ timecomplexity_o(n).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+int main() 
+{
+    long n,i;
+    clock_t start,end;
+    double time_taken;
+    printf("enter n :\n ");
+    scanf("%ld",&n);
+    start=clock();
+    for(i=0;i<n;i++)
+    {
+        int x=i*2;
+    }
+    end=clock();
+    time_taken=((double)(end-start));
+    printf(" O(n) time = %f seconds \n",time_taken);
+    return 0;
+}
+    
+
