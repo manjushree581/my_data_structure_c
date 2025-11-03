33503665e06940c9f281581dc52b17e25a57c1c3
manjushree581 <manjushreepb14@gmail.com>
2025-11-03 11:57:30 +0530

    malloc.c

---
+++ malloc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr= (int*)malloc(20);
+    for(int i=0;i<5;i++)
+    ptr[i]=i+1;
+    for(int i=0;i<5;i++)
+    printf("%d",ptr[i]);
+}
