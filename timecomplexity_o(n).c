8260bbc6573bddbc73b8449adde45c99060b621d
manjushree581 <manjushreepb14@gmail.com>
2025-11-03 12:54:01 +0530

    timecomplexity_o(n).c

--- timecomplexity_o(n).c
+++ timecomplexity_o(n).c
@@ -13,7 +13,7 @@ int main()
         int x=i*2;
     }
     end=clock();
-    time_taken=((double)(end-start));
+    time_taken=((double)(end-start))/CLOCKS_PER_SEC;
     printf(" O(n) time = %f seconds \n",time_taken);
     return 0;
 }
