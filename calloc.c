72639e1b9ef45818e370df210a53249c889eb643
manjushree581 <manjushreepb14@gmail.com>
2025-11-03 11:58:13 +0530

    calloc.c

---
+++ calloc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr=(int*)calloc(5,sizeof(int));
+    if(ptr==NULL)
+    {
+    printf("allocation failed\n");
+    exit(0);
+    }
+    for(int i=0;i<5;i++)
+    printf("%d",ptr[i]);
+    return 0;
+}
